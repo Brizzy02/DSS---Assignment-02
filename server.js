9cd48f0456512cd05953173b7c3d601b74952b27
Nonthapat1 <cower987654321@gmail.com>
2024-03-21 02:10:55 +0000

    add

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+	res.redirect("index");
+});
+
+app.get("/editpost", (req, res) => {
+	res.render("editpost");
+});
+
+app.get("/newpost", (req, res) => {
+	res.render("newpost");
+});
+
+
+app.listen(3000)
