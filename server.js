4c04e7e337a327b628c0dcb0811ee0e8a15149e9
alaba98 <83726565+alaba98@users.noreply.github.com>
2024-05-07 14:54:34 +0100

    foundation

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // limit each IP to 100 requests per windowMs
+    max: 300, // limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again after an hour'
    });
 
